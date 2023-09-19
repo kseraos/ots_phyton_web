09447466b193707f7c25b25a7530c90fc75dccba
Kristine Santana <kseraos@outlook.com>
2023-09-19 18:06:34 +0000

    login funcionando

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g,  request, session, abort, flash, redirect, url_for
 
 app = Flask("Meu app")
-
+app.config['SECRET_KEY'] = 'pudim'
 
 posts = [
     {
@@ -18,3 +18,14 @@ posts = [
 def exibir_entradas():
     entradas = posts #Mock das postagens
     return render_template('exibir_entradas.html', entradas=entradas)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = None
+    if request.method == "POST":
+        if request.form['username'] == "admin" and request.form['password'] == "admin":
+            session['logado'] = True
+            flash("Login efetuado com sucesso!")
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou senha inválidos"        
+    return render_template('login.html', erro=erro)
