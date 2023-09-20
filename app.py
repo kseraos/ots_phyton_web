272b935bd819f452ed5d7e4b563a741226904880
Kristine Santana <kseraos@outlook.com>
2023-09-20 19:49:01 +0000

    Feat: Criadas variaveis e função para a fase de dados e o import do sqlite

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, g,  request, session, abort, flash, redirect, url_for
 from posts import posts
-app = Flask("Meu app")
+import sqlite3
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'pudim'
+
+app.config.from_object(__name__)
+DATABASE = "banco.bd"
+
+def conectar():
+    return sqlite3.connect(DATABASE)
 
 
 @app.route('/')
