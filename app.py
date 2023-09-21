81b065179872acd6e9e19d80592068423468da65
Kristine Santana <kseraos@outlook.com>
2023-09-21 15:46:03 +0000

    Feat: import class G, Fae dict de dados e select

--- app.py
+++ app.py
@@ -5,16 +5,35 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'pudim'
 
 app.config.from_object(__name__)
+
 DATABASE = "banco.bd"
 
 def conectar():
     return sqlite3.connect(DATABASE)
 
+@app.before_request
+def before_request():
+    g.bd = conectar()    
+
+@app.teardown_request
+def teardown_request(f):
+    g.bd.close()   
+
 
 @app.route('/')
 def exibir_entradas():
-    entradas = posts[::-1] #Mock das postagens
-    return render_template('exibir_entradas.html', entradas=entradas)
+    # entradas = posts[::-1] #Mock das postagens
+
+    sql = "SELECT titulo, texto, data_criacao FROM posts ORDER BY id DESC"
+    resultado = g.bd.execute(sql)
+
+    entrada = [
+        {"titulo": "Primeiro Titulo", "texto": "Primeiro",  "data_criacao": "11/09/2023"},
+        {"titulo": "Segundo Titulo", "texto": "Segundo",  "data_criacao": "12/09/2023"}
+    ]
+
+
+    return render_template('exibir_entradas.html', entradas=entrada)
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -47,8 +66,8 @@ def inserir_entradas():
 @app.route('/posts/<int:id>')
 def exibir_entrada(id):
     try:
-        entrada = posts[id-1]
-        return render_template('exibir_entrada.html', entrada=entrada)
+        entradas = posts[id-1]
+        return render_template('exibir_entrada.html', entradas=entradas)
     except Exception:
         return abort(404)
 
